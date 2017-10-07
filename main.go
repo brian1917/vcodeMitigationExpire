2714057843896c4cab19e21f2ffc2810033fac0f
Brian Pitta <brianpitta@gmail.com>
2017-10-06 20:46:20 -0400

    removed some debugging code

--- main.go
+++ main.go
@@ -4,33 +4,33 @@ import (
 	"fmt"
 	"github.com/brian1917/vcodeapi"
 	"log"
-	"strings"
 	"os"
-	"reflect"
+	"strings"
 )
 
 func main() {
 
+	// SET UP LOGGING FILE
 	f, err := os.OpenFile("vcodeMitigationExpire.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	fmt.Println(reflect.TypeOf(f))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-
 	log.SetOutput(f)
 	log.Printf("Started running")
 
+	// SET SOME VARIABLES
 	var appSkip bool
 	var flaws []vcodeapi.Flaw
 	var recentBuild string
 	var errorCheck error
 	var flawList []string
 
+	// PARSE CONFIG FILE
 	config := parseConfig()
 
+	// GET APP LIST
 	appList := getApps(config.Auth.User, config.Auth.Password, config.AppScope.LimitAppList, config.AppScope.AppListTextFile)
-
 	appCounter := 0
 
 	// CYCLE THROUGH EACH APP
@@ -78,16 +78,16 @@ func main() {
 					//THE MOST RECENT MITIGATION ACTION IS THE ACCEPTANCE, PROPOSAL SHOULD BE SECOND LAST IN ARRAY
 					recentProposal := f.Mitigations.Mitigation[len(f.Mitigations.Mitigation)-2]
 					recentApproval := f.Mitigations.Mitigation[len(f.Mitigations.Mitigation)-1]
-					// RUN CHECKS FOR MITIGATION TYPE
+					// CHECK FOR MITIGATION TYPE
 					if (recentProposal.Action == "Potential False Positive" && config.TargetMitigations.PotentialFalsePositive == true) ||
 						(recentProposal.Action == "Mitigate by Design" && config.TargetMitigations.MitigatedByDesign == true) ||
 						(recentProposal.Action == "Mitigate by Network Environment" && config.TargetMitigations.MitigatedByNetworkEnv == true) ||
 						(recentProposal.Action == "Mitigate by OS Environment" && config.TargetMitigations.MitigationByOSEnv == true) ||
 						(recentProposal.Action == "Reviewed - No Action Taken" && config.TargetMitigations.ReviewedNoActionTaken == true) ||
 						(recentProposal.Action == "Remediated by User" && config.TargetMitigations.RemediatedByUser == true) {
-						// RUN CHECK FOR INCLUDING COMMENT TEXT
+						// CHECK FOR INCLUDING COMMENT TEXT
 						if (config.CommentText.RequireCommentText == true && strings.Contains(recentApproval.Description, config.CommentText.Text)) ||
-							(config.CommentText.RequireCommentText == false){
+							(config.CommentText.RequireCommentText == false) {
 							// CHECK IF EXPIRED AND BUILD ARRAY
 							if expireCheck(f, config) == true {
 								flawList = append(flawList, f.Issueid)
@@ -98,14 +98,11 @@ func main() {
 			}
 			// IF WE HAVE FLAWS MEETING CRITERIA, RUN UPDATE MITIGATION API
 			if len(flawList) > 0 {
-				/*vcodeapi.ParseUpdateMitigation(config.Auth.User, config.Auth.Password, recentBuild,
-					"reject", "Expired", strings.Join(flawList, ","))*/
+				vcodeapi.ParseUpdateMitigation(config.Auth.User, config.Auth.Password, recentBuild,
+					"reject", "Expired", strings.Join(flawList, ","))
 				log.Printf("App ID %v: Reject Flaw IDs %v\n", appID, strings.Join(flawList, ","))
 			}
 		}
 	}
 	log.Printf("Completed running")
-	if 2==3{
-		fmt.Println(recentBuild)
-	}
 }
